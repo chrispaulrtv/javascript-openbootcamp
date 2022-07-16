e70f587b72ca6bbfd3c00f76c0bd6be3ac4ee102
ChrisPaulRTV <creinoso7982@gmail.com>
2022-07-16 17:01:27 -0500

    Resolución Ejercicio 2 Javascript Básico OpenBootCamp

---
+++ SintaxisVariablesPalabrasResevadas/index.js
@@ -0,0 +1,14 @@
+const lista = [
+  'ChrisPaulRTV',
+  28,
+  true,
+  new Date('08-31-1993'),
+  {
+    titulo: 'Los 7 hábitos de la gente altamente efectiva',
+    autor: 'Stephen R. Covey',
+    fecha: new Date('01-03-2011'),
+    url: 'https://www.amazon.com/-/es/Stephen-R-Covey/dp/9688533769/ref=sr_1_3?crid=2SJRFBA2B5RD5&keywords=los+7+habitos+de+la+gente+altamente+efectiva&qid=1658008670&sprefix=los+7+habit%2Caps%2C193&sr=8-3',
+  },
+]
+
+console.log(lista)
