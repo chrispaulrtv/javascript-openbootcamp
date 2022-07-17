a10a3cf585d3c761f9bbd4536c22b6eb0341c8fd
ChrisPaulRTV <creinoso7982@gmail.com>
2022-07-17 17:39:59 -0500

    Resolución ejercicio de números en javascript básico OpenBootCamp

---
+++ Numeros/index.js
@@ -0,0 +1,15 @@
+let alturaCm = parseInt(169)
+
+let alturaM = parseFloat(1.69)
+
+let pesoKg = parseFloat(69.8)
+
+const alturaRound = Math.ceil(alturaM)
+
+const pesoRound = Math.floor(pesoKg)
+
+const maximosIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(alturaRound)
+console.log(pesoRound)
+console.log(maximosIguales)
