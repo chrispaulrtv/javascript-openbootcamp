59ad851dfb12b863c4b5782323d2da4d8f5fe156
ChrisPaulRTV <creinoso7982@gmail.com>
2022-07-17 15:34:22 -0500

    Resolución ejercicio 4 Cadenas de Texto Javascript Básico OpenBootCamp

---
+++ CadenasDeTexto/index.js
@@ -0,0 +1,23 @@
+let nombre = 'Christian'
+
+let apellido = 'Reinoso'
+
+let estudiante = nombre + ' ' + apellido
+
+let estudianteMayus = estudiante.toUpperCase()
+
+let estudianteMinus = estudiante.toLowerCase()
+
+let longitudEstudiante = estudiante.length
+
+let primeraLetra = estudiante.charAt(0)
+
+let ultimaLetra = estudiante.charAt(estudiante.length - 1)
+
+let eliminaEspacios = estudiante.replace(/ /g, '')
+
+// let existeNombre = estudiante.includes(nombre);
+let existeNombre = estudiante.includes('Christian');
+
+
+
